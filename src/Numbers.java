fabdf96e035b172d4e668299a8692270f6fa9122
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-05-19 21:51:54 +0200

    Stworzenie dziwnego kalkulatora

---
+++ src/Numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+        if (number1 > number1){
+            System.out.println(number1+number2);
+        }else if (number1<number2){
+            System.out.println(number1*number2);
+        }else if (number1 == number2){
+            System.out.println(number1*number1+number2*number2);
+        }
+
+
+
+}}
